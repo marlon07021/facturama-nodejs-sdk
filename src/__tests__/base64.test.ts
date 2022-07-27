93d2307fc88aa621d905f4afc9cab1f721c30a64
Marlon Tornes Torres <marlon07021@gmail.com>
2022-07-27 10:33:16 -0500

    [ADD] QS and Base64 test.

---
+++ src/__tests__/base64.test.ts
@@ -0,0 +1,9 @@
+import {encodeBase64, decodeBase64} from "../util/base64";
+
+test('base64', () => {
+    const testString = 'abc';
+    const encoded = 'YWJj';
+    expect(encodeBase64(testString)).toBe(encoded);
+    expect(decodeBase64(encodeBase64(testString))).toBe(testString);
+
+});
---
+++ src/__tests__/qs.test.ts
@@ -0,0 +1,17 @@
+import {encodeParams} from "../util/qs";
+
+test('qs', () => {
+   const queryObject = {
+       query: 'example'
+   };
+   const queryString = "query=example";
+   expect(encodeParams(queryObject)).toBe(queryString);
+
+    const multiQueryObject = {
+        query: 'example',
+        query2: 'example2'
+    };
+    const multiQueryString = "query=example&query2=example2";
+
+    expect(encodeParams(multiQueryObject)).toBe(multiQueryString);
+});
